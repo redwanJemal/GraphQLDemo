9a1f5c360a62041ae74253dead1388465bbf82d8
Redwan Mohammed <redjemal2@gmail.com>
2021-11-14 11:34:18 +0530

    GraphQL Demo

---
+++ GraphQL/Query.cs
@@ -0,0 +1,28 @@
+using System.Linq;
+using CommanderGQL.Data;
+using CommanderGQL.Models;
+using HotChocolate;
+using HotChocolate.Data;
+
+namespace CommanderGQL.GraphQL
+{
+    public class Query
+    {
+        [UseDbContext(typeof(AppDbContext))]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Platform> GetPlatforms([ScopedService] AppDbContext context)
+        {
+            return context.Platforms;
+        }
+
+        [UseDbContext(typeof(AppDbContext))]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Command> GetCommand([ScopedService] AppDbContext context)
+        {
+            return context.Commands;
+        }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommanderGQL.Data;
+using CommanderGQL.GraphQL;
+using CommanderGQL.GraphQL.Commands;
+using CommanderGQL.GraphQL.Platforms;
+using GraphQL.Server.Ui.Voyager;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace COMMANDERGQL
+{
+    public class Startup
+    {
+        public readonly IConfiguration _config;
+        private readonly IWebHostEnvironment _env;
+        public Startup(IConfiguration config,IWebHostEnvironment env)
+        {
+            _config = config;
+            _env = env;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddPooledDbContextFactory<AppDbContext>(opt => opt.UseSqlServer(_config.GetConnectionString("CommandConnStr")));
+            services
+                .AddGraphQLServer()
+                .AddQueryType<Query>()
+                .AddMutationType<Mutation>()
+                .AddSubscriptionType<Subscription>()
+                .AddType<PlatformType>()
+                .AddType<AddPlatformInputType>()
+                .AddType<AddPlatformPayloadType>()
+                .AddType<CommandType>()
+                .AddType<AddCommandInputType>()
+                .AddType<AddCommandPayloadType>()
+                .AddFiltering()
+                .AddSorting()
+                .AddInMemorySubscriptions()
+                .ModifyRequestOptions(opt => opt.IncludeExceptionDetails = _env.IsDevelopment());  
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseWebSockets();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGraphQL();
+            });
+
+            app.UseGraphQLVoyager(new GraphQLVoyagerOptions()
+            {
+                GraphQLEndPoint = "/graphql",
+                Path = "/graphql-voyager"
+            });
+        }
+    }
+}
